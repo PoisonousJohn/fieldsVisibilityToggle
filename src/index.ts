bd05aba0466f333f9ca20223d31764a4187a810c
Ivan Fateev <ivan@anna.money>
2020-06-17 12:10:16 +0300

    initial impl

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export class Lib {
+export function updateOnSelect(select: JQuery<any>, visibleValue: string, fields: JQuery<any>[]) {
+    select.on('change', () => {
+        let visible = select.val() === visibleValue
+        fields.forEach(element => {
+            visible ? element.show() : element.hide()
+        })
+    })
 }
