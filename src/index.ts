f9c3de37eafa21292b59e62a82e65884fd768202
Ivan Fateev <ivan@anna.money>
2020-06-17 12:33:17 +0300

    include proper value handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export function updateOnSelect(select: JQuery<any>, visibleValue: string, fields: JQuery<any>[]) {
     select.on('change', () => {
-        let visible = select.val() === visibleValue
+        let visible = select.filter(function () {
+            let el = jQuery(this)
+            return el.is(':checked') || el.is(':selected')
+        }).val() === visibleValue
+
         fields.forEach(element => {
             visible ? element.show() : element.hide()
         })
